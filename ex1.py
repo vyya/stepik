b023130267d7eb2ea9aeff789eb4ccc1f488d534
neuro <yamsh@yandex.ru>
2024-10-01 16:18:58 +0500

    assigning variables and python help

---
+++ ex1.py
@@ -0,0 +1,26 @@
+# assigning variables
+# dynamic data typing
+a = 7
+a = 6.8
+# variable does not keep a value, it refers to an object - value
+b = a
+b = 'hello'
+b = -8.5
+# cascade variable assigning
+a = b = c = 0
+# multiple assignment
+a,b = 7.1,9.2
+# values exchange
+a,b = b,a 
+var_a = 'hello'
+print(a, b, c)
+print(id(a), id(b), id(c))
+print(b)
+print(var_a)
+print(type(b))
+
+x = 5.8
+s = 'helloween'
+print(type(x), type(s))
+
+# python console - help() - keywords to look at the service keywords
